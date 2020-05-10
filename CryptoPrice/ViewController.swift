de46675c8cffef2f2b3117e7003fdac68a12267a
Jan <jsvensson51@yahoo.com>
2020-05-10 10:13:17 +0200

    Initial Commit

---
+++ CryptoPrice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoPrice
+//
+//  Created by Jan Svensson on 2020-05-10.
+//  Copyright © 2020 Jan Svensson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CryptoPriceTests/CryptoPriceTests.swift
@@ -0,0 +1,34 @@
+//
+//  CryptoPriceTests.swift
+//  CryptoPriceTests
+//
+//  Created by Jan Svensson on 2020-05-10.
+//  Copyright © 2020 Jan Svensson. All rights reserved.
+//
+
+import XCTest
+@testable import CryptoPrice
+
+class CryptoPriceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
